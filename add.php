f4f363f0c58fea42f09c3e9a777365de07097706
WhidanB <danyphone95@gmail.com>
2023-05-10 10:24:58 +0200

    Formulaire nom DB ok 	modified:   add.php 	modified:   stagiaire.php 	modified:   style.css

--- add.php
+++ add.php
@@ -1,5 +1,21 @@
 <?php
 
+if ($_POST) {
+    if (
+        isset($_POST['first_name'])
+        && isset($_POST['last_name'])
+    ) {
+        // print_r($_POST);
+        require_once('connect.php');
+        $first_name = strip_tags($_POST['first_name']);
+        $last_name = strip_tags($_POST['last_name']);
+        $sql = "INSERT INTO stagiaire (first_name, last_name) VALUES (:first_name, :last_name)";
+        $query = $db->prepare($sql);
+        $query->bindValue(':first_name', $first_name, PDO::PARAM_STR_CHAR);
+        $query->bindValue(':last_name', $last_name, PDO::PARAM_STR_CHAR);
+        $query->execute();
+    }
+}
 
 ?>
 
@@ -10,16 +26,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 
 <body>
     <h1>Ajouter un stagiaire</h1>
-    <form action="">
+    <form method="post">
         <div>
             <label for="first_name">First name</label>
             <input type="text" name="first_name" required>
-            <label for="last_name">First name</label>
+            <label for="last_name">Last name</label>
             <input type="text" name="last_name" required>
             <input type="submit" value="send">
         </div>
--- stagiaire.php
+++ stagiaire.php
@@ -26,6 +26,7 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title><?= $result['first_name'] . ' ' . $result['last_name'] ?></title>
 </head>
 
